b147671f984db5fba940b39dc34b3fa4a5e18e49
Mighty <flow.energy@gmail.com>
2011-07-04 16:47:42 +0400

    Types -> Taxons update
    Taxon adding functionality incoming.

--- app/controllers/admin/types_controller.rb
+++ app/controllers/admin/types_controller.rb
@@ -1,45 +1,58 @@
 class Admin::TypesController < Admin::ResourceController
   def new
     render :layout => !request.xhr?
-	@type = Type.new
+  	@type = Type.new
   end
   
   def create
-	@type ||= params[:type]
-	if @type.save
-	  redirect_to :admin_types
-	else
-	  redirect_to :new_admin_type
-	end
+  	@type ||= params[:type]
+  	if @type.save
+  	  redirect_to :admin_types
+  	else
+  	  redirect_to :new_admin_type
+  	end
   end
 
   def destroy
-	@type = Type.find(params[:id])
-	if @type.destroy
-	  flash[:notice] = I18n.t('resource_controller.successfuly_removed')
-	  respond_with(@type) do |format|
-        format.html { redirect_to collection_url }
-        format.js   { render :partial => "/admin/shared/destroy" }
-      end
-	else
-	  flash[:notice] = t('error')
-	  respond_with(@type) do |format|
-		format.html { redirect_to collection_url }
-	  end
-	end
+  	@type = Type.find(params[:id])
+  	if @type.destroy
+  	  flash[:notice] = I18n.t('resource_controller.successfuly_removed')
+  	  respond_with(@type) do |format|
+          format.html { redirect_to collection_url }
+          format.js   { render :partial => "/admin/shared/destroy" }
+        end
+  	else
+  	  flash[:notice] = t('error')
+  	  respond_with(@type) do |format|
+  		format.html { redirect_to collection_url }
+  	  end
+  	end
   end
   
   def edit
-	@type = Type.find(params[:id])
-	@taxons = @type.taxons
-	@products_count = Hash.new
-	@taxons.each do |taxon|
-	  @products_count[taxon.id] = Product.joins(:taxons).where('taxons.parent_id' => taxon.id).count
+  	@type = Type.find(params[:id])
+  	@taxons = @type.taxons
+  	@products_count = Hash.new
+  	@taxons.each do |taxon|
+  	  @products_count[taxon.id] = Product.joins(:taxons).where('taxons.parent_id' => taxon.id).count
+    end
   end
-  logger.info @products_count
+  
+  # ajax method to add taxon to type
+  def add
+  end
+  
+  # ajax method to remove taxon from type
+  def remove
   end
   
   def index
     @types = Type.all
+  end
+  
+  protected
+  # # PRIMARY GOAL
+  def available_taxons
+    Taxon.where(:parent_id => nil, :id => @taxons)
   end
 end
