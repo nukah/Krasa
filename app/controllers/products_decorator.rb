58ec0a56661915edc138c301363d02f0d52797d7
Mighty <flow.energy@gmail.com>
2011-07-07 18:09:26 +0400

    fixes

--- app/controllers/products_decorator.rb
+++
@@ -1,4 +0,0 @@
-ProductController.class_eval do
-  @product_properties = ProductProperty.find_all_by_product_id(@product.id,
-                          :include => [:property], :conditions => ['properties.visibility = ?', true])
-end
