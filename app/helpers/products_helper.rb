da7644249574182040e9b87cf33f54d5e770b6e6
Nuke <flow.energy@gmail.com>
2011-08-16 18:04:25 +0400

    helper method for bestsellers added
    base sql written

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -7,4 +7,9 @@ module ProductsHelper
     amount += Calculator::Vat.calculate_tax_on(product_or_variant) if Spree::Config[:show_price_inc_vat]
     options.delete(:format_as_currency) ? format_price(amount, options) : amount
   end
+
+  def product_bestsellers(count, taxon = nil,start = 1.week.ago,finish = Time.now)
+    SQL = "select v.product_id,sum(li.quantity) sum from orders ord join line_items li on li.order_id = ord.id and ord.state == 'complete' join variants v on v.id = li.variant_id group by v.product_id;"
+    #TODO
+  end
 end
--- app/helpers/taxonomies_helper.rb
+++ app/helpers/taxonomies_helper.rb
@@ -2,5 +2,6 @@ module TaxonomiesHelper
   def get_current_menu
     if current_page?(:root)
       @taxonomies = Type.where(:first => true).taxonomies
+    end
   end
 end
